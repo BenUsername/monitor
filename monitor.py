55c649b8b1e008cf139ee04000b7e0a0e1ceadbe
Ben <ben.tannenbaum1@gmail.com>
2018-12-05 12:26:49 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,3 +1,6 @@
+# In this script we scrap two websites, one with a list of companies, one with recent news, and check whether any news applies to our 
+# list of company. If that is the case, we send an email indicating that fact. 
+
 import os
 os.system('cls' if os.name == 'nt' else 'clear') # clears terminal for both windows and Unix
 
@@ -13,11 +16,13 @@ import time
 # Import smtplib (to allow us to email)
 import smtplib
 
+# *** SCRAP LIST OF NEWS *** 
+
 list_recent = []
 text = "start"
 
 for k in range (0,5+1):
-    # set the url as VentureBeat,
+    # set the url 
     url = f"https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=&SIC=&State=&Country=&CIK=&owner=only&accno=&start={k*100}&count=100"
     # set the headers like we are a browser,
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -34,30 +39,15 @@ for k in range (0,5+1):
     else:
         list_recent += text
 
+        
+# saves the list of news as html so that we can potentially work on it without re-scraping, reducing risk of ban from websites        
 with open("list_recent.html", "w") as file:
     file.write(str(list_recent))
 
+# *** SCRAP LIST OF COMPANIES ***    
+    
 list_names = []
 names = "start"
-
-for k in range(0,10+1):
-
-    url = f"https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&SIC=2836&owner=include&match=&start={k*100}&count=100&hidefilings=0"
-    # set the headers like we are a browser,
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    # download the homepage
-    response = requests.get(url, headers=headers)
-    html = response.text
-    # parse the downloaded homepage and grab all text, then,
-    soup = BeautifulSoup(html, "lxml")
-
-    names = [td.text for td in soup.find_all('td')]
-
-    if names == []:
-        break
-
-    else:
-        list_names += names
 
 for k in range(0,10+1):
 
@@ -78,14 +68,19 @@ for k in range(0,10+1):
     else:
         list_names += names
 
+# saves the list of news as html so that we can potentially work on it without re-scraping, reducing risk of ban from websites   
+        
 with open("list_names.html", "w") as file:
     file.write(str(list_names))
 
+# *** MATCH & EMAIL *** 
+# checks if list of news mentions one of the companies and send an email if that is the case
 
 for company in list_recent:
     if company in list_names:
         print(f"{company} has recent news")
 
+        
         # create an email message with just a subject line,
         msg = f'Subject: This is Ben\'s script talking, {company} has recent news'
         # set the 'from' address,
@@ -97,7 +92,7 @@ for company in list_recent:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         # add my account login name and password,
-        server.login("maxbaude0@gmail.com", "professeur7")
+        server.login("maxbaude0@gmail.com", ********) #replace with email p/word when using script
 
         # Print the email's contents
         print('From: ' + fromaddr)
